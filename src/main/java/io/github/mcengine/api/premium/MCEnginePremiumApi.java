0175565fe8644162ba81e7a93ea65bbbb7c6ed26
JetsadaWijit <jetsadawijit@outlook.com>
2025-06-13 05:42:57 +0000

    feat: Add getAPI and getPlugin functions

--- src/main/java/io/github/mcengine/api/premium/MCEnginePremiumApi.java
+++ src/main/java/io/github/mcengine/api/premium/MCEnginePremiumApi.java
@@ -1,3 +1,22 @@
 package io.github.mcengine.api.premium;
 
-public class MCEnginePremiumApi {}
+import org.bukkit.plugin.Plugin;
+
+public class MCEnginePremiumApi {
+
+    private static MCEnginePremiumApi instance;
+    private final Plugin plugin;
+
+    public MCEnginePremiumApi(Plugin plugin) {
+        instance = this;
+        this.plugin = plugin;
+    }
+
+    public static MCEnginePremiumApi getApi() {
+        return instance;
+    }
+
+    public Plugin getPlugin() {
+        return plugin;
+    }
+}
